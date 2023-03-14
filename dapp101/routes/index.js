6502368347493a7ada6c164b8528e6e1eeb8786d
moondda <dahyun723@ajou.ac.kr>
2023-03-14 11:07:18 +0900

    feature:nft판매페이지,token판매페이지

--- dapp101/routes/index.js
+++ dapp101/routes/index.js
@@ -1,17 +1,24 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Dapp tutordfial' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Dapp tutordfial" });
 });
 
-router.get('/ico', function(req, res, next) {
-  res.render('ico', { title: 'ICO tutordfial' });
+router.get("/ico", function (req, res, next) {
+  res.render("ico", { title: "ICO tutordfial" });
 });
 
+router.get("/nft", function (req, res, next) {
+  res.render("nft", { title: "nft tutordfial" });
+});
 
-router.get('/nft', function(req, res, next) {
-  res.render('nft', { title: 'nft tutordfial' });
+router.get("/token", function (req, res, next) {
+  res.render("token", { title: "nft tutordfial" });
+});
+
+router.get("/vote", function (req, res, next) {
+  res.render("vote", { title: "nft tutordfial" });
 });
 module.exports = router;
