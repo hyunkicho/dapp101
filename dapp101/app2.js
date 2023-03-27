76f49c46af836e49e24fcdd2a92def485b17e794
moondda <dahyun723@ajou.ac.kr>
2023-03-27 23:32:36 +0900

    page edit form&console

---
+++ dapp101/app2.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("token");
+});
+
+app.post("/submit", (req, res) => {
+  const inputValue = req.body.inputValue;
+  console.log(inputValue);
+  res.redirect("/");
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
