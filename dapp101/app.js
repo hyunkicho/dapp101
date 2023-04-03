6aa3fb52565e42b8353c747f216d1b90bae0defe
moondda <dahyun723@ajou.ac.kr>
2023-04-04 03:51:52 +0900

    feat:put nftnumber&show attribute

--- dapp101/app.js
+++ dapp101/app.js
@@ -6,6 +6,7 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+const router = require("./routes/index");
 
 var app = express();
 
--- dapp101/routes/index.js
+++ dapp101/routes/index.js
@@ -1,7 +1,36 @@
 var express = require("express");
 var router = express.Router();
+const axios = require("axios");
 
 /* GET home page. */
+
+// router.get("/nftnumber", async (req, res, next) => {
+//   try {
+//     let number = req.query.number;
+//     const response = await axios.get(
+//       `https://raw.githubusercontent.com/dogeum-network/nft-baseuri/main/metadatas/${number}`
+//     );
+//     const data = response.data;
+//     res.render("nft", { data: data });
+//   } catch (error) {
+//     console.error(error);
+//     res.send("Error");
+//   }
+// });
+
+router.get("/nftnumber", (req, res) => {
+  res.render("nftnumber");
+});
+
+router.get("/nft", async (req, res) => {
+  let number = req.query.number;
+  const response = await axios.get(
+    `https://raw.githubusercontent.com/dogeum-network/nft-baseuri/main/metadatas/${number}`
+  );
+  const data = response.data;
+  res.render("nft", { data: data });
+});
+
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Dapp tutordfial" });
 });
@@ -16,6 +45,10 @@ router.get("/nft", function (req, res, next) {
 
 router.get("/token", function (req, res, next) {
   res.render("token", { title: "nft tutordfial" });
+});
+
+router.get("/nftnumber", function (req, res, next) {
+  res.render("nftnumber", { title: "nft tutordfial" });
 });
 
 // router.get("/vote", function (req, res, next) {
